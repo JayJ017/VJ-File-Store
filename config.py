e09465227284a5242ead48c80713cf538344f9d0
JayJ017 <mickypoo49@gmail.com>
2024-06-10 18:20:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ BOT_USERNAME = environ.get("BOT_USERNAME", "choco_supplier_bot") # your bot user
 PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "")) # Time in Seconds
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "00")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "00")) # Time in Seconds
 
 # Channel Information
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002117718711"))
